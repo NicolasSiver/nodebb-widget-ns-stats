95223f5fade328195bd9c6a01d955dcf789a1753
Nicolas Siver <nicolas@siver.im>
2016-01-05 20:38:30 -0500

    users module

---
+++ widget/users.js
@@ -0,0 +1,42 @@
+(function (Users) {
+    'use strict';
+
+    var async = require('async');
+
+    var logger = require('./logger'),
+        nodebb = require('./nodebb'),
+        db     = nodebb.db,
+        user   = nodebb.user;
+
+    var online = [],
+        today  = [];
+
+    Users.getOnline = function (done) {
+        return done(null, online);
+    };
+
+    Users.getToday = function (done) {
+        return done(null, today);
+    };
+
+    Users.online = function (uid, done) {
+        if (!today[uid]) {
+            user.getUserData(uid, function (error, userData) {
+                if (error) {
+                    return done(error);
+                }
+                today[uid] = userData;
+                logger.log('verbose', 'User %d is online', uid);
+                done(null);
+            });
+        } else {
+            done(null);
+        }
+    };
+
+    Users.reset = function (done) {
+        visited.length = 0;
+        done(null);
+    };
+
+})(module.exports);
